735094d74c59d7f483e9a4a50a0675e19949c59b
Maximilian Arzberger <m@mgp.sh>
2026-04-02 23:33:58 +0200

    fix

--- ocpp-proxy/run.sh
+++ ocpp-proxy/run.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-source /usr/lib/bashio/bashio.sh
 
-bashio::log.info "Starting OCPP Proxy..."
+echo "[$(date '+%H:%M:%S')] INFO: Starting OCPP Proxy..."
 
-# Change to application directory and set PYTHONPATH
 cd /app
 export PYTHONPATH=/app/src
 
-# Start the OCPP Proxy
 exec python3 -m ocpp_proxy.main
